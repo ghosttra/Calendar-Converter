ca7c2cacc1b75f9f5623ae0855e96caaa39b0ca2
tr1ckshxt <skoromny.m@gmail.com>
2022-07-11 17:10:12 +0300

    Добавьте файлы проекта.

---
+++ Calendar-Converter/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Calendar_Converter
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Calendar_OnSelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (Calendar.SelectedDate != null)
+                Calendar.DisplayDate = (DateTime)Calendar.SelectedDate;
+        }
+    }
+
+    public class CalendarConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return null;
+            string _value = value.ToString();
+            if (_value != string.Empty)
+            {
+                DateTime dt;
+                if (DateTime.TryParse(_value, out dt)) return dt;
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return $"{(value as DateTime? ?? default).Date}";
+        }
+
+    }
+
+    public class CalendarValidator : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            DateTime dt;
+            string date = (value as string);
+            if (DateTime.TryParse(date, out dt))
+                return ValidationResult.ValidResult;
+            return new ValidationResult(false, null);
+        }
+    }
+}
